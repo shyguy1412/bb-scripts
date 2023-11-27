bb260a0e1e4774b010314ee14a7ac851fd75e451
Nasram <48560522+NilsRamstoeck@users.noreply.github.com>
2023-11-27 11:50:59 +0100

    Initial commit

---
+++ build.mjs
@@ -0,0 +1,30 @@
+import { context } from 'esbuild';
+import { BitburnerPlugin } from 'esbuild-bitburner-plugin';
+
+const createContext = async () => await context({
+  entryPoints: [
+    'servers/**/*.js',
+    'servers/**/*.jsx',
+    'servers/**/*.ts',
+    'servers/**/*.tsx',
+  ],
+  outbase: "./servers",
+  outdir: "./build",
+  plugins: [
+    BitburnerPlugin({
+      port: 12525,
+      types: 'NetscriptDefinitions.d.ts',
+      mirror: {
+      },
+      distribute: {
+      }
+    })
+  ],
+  bundle: true,
+  format: 'esm',
+  platform: 'browser',
+  logLevel: 'info'
+});
+
+let ctx = await createContext();
+ctx.watch();
