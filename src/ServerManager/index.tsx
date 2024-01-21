1e345dfb527eee6f4dc7d882dca46b29089e2e14
Nils Ramstoeck <nilsramstoeck@gmail.com>
2024-01-21 16:36:08 +0000

    refactor server manager

---
+++ src/ServerManager/index.tsx
@@ -0,0 +1,62 @@
+import Style from './ServerManager.css';
+import { ServerEntry } from './ServerEntry';
+import { List } from '@/lib/components/List';
+import { getPurchasedServers } from '@/lib/util/Network';
+import { Server } from 'NetscriptDefinitions';
+import React, { useState, useRef } from 'react';
+
+type Props = {
+  ns: NS;
+};
+
+export function ServerManager({ ns }: Props) {
+
+  const [servers, setServers] = useState<Server[]>(getPurchasedServers(ns));
+  const inputRef = useRef<HTMLInputElement>(null);
+  const defaultNewServerName = `grindr-${+servers.at(-1).hostname.split('-')[1] + 1}`;
+
+  return <>
+    <Style></Style>
+    <div className='server-manager-layout'>
+
+      <div className='server-manager-purchase-server'>
+        <span>
+          Purchase new server:
+          &nbsp;
+          <input type='text' ref={inputRef} defaultValue={defaultNewServerName} key={defaultNewServerName} />
+          &nbsp;
+        </span>
+        <span className='server-manager-purchase-server-button'
+          onClick={() => {
+            'use getPurchasedServerLimit';
+            'use purchaseServer';
+
+            if (!inputRef.current) throw new Error('Something went wrong, no input ref');
+            const newServer = ns.purchaseServer(inputRef.current.value, 2);
+            if (!newServer && servers.length == ns.getPurchasedServerLimit()) {
+              ns.toast('Server limit has been reached', 'error');
+              return;
+            } else if (!newServer) {
+              ns.toast('Not enough money', 'error');
+              return;
+            }
+            ns.toast(`Successfully bought server '${newServer}'`, 'success');
+            setServers(getPurchasedServers(ns));
+          }}
+        >OK</span>
+      </div>
+      <div className='server-manager'>
+        {React.createElement(List,)}
+        <List li={ServerEntry} data={servers.map(s => ({
+          ns,
+          server: s,
+          remove: () => {
+            'use deleteServer';
+            ns.deleteServer(s.hostname);
+            setServers(getPurchasedServers(ns));
+          }
+        }))}></List>
+      </div>
+    </div>
+  </>;
+}
--- src/servers/home/ServerManager.tsx
+++ src/servers/home/ServerManager.tsx
@@ -1,67 +1,9 @@
-import Style from '@/ServerManager/ServerManager.css';
-import { ServerEntry } from '@/ServerManager/components/ServerEntry';
+import { ServerManager } from '@/ServerManager';
 import { createWindowApp } from '@/lib/WindowApp';
-import { List } from '@/lib/components/List';
-import { getPurchasedServers } from '@/lib/util/Network';
-import { Server } from 'NetscriptDefinitions';
-import React, { useRef, useState } from 'react';
+import React, {  } from 'react';
 
 export async function main(ns: NS) {
   const WindowApp = createWindowApp(ns);
 
-  return WindowApp.mount(<WindowComponent ns={ns}></WindowComponent>);
-}
-
-type Props = {
-  ns: NS;
-};
-
-function WindowComponent({ ns }: Props) {
-  const [servers, setServers] = useState<Server[]>(getPurchasedServers(ns));
-  const inputRef = useRef<HTMLInputElement>(null);
-  const defaultNewServerName = `grindr-${+servers.at(-1).hostname.split('-')[1] + 1}`;
-  return <>
-    <Style></Style>
-    <div className='server-manager-layout'>
-
-      <div className='server-manager-purchase-server'>
-        <span>
-          Purchase new server:
-          &nbsp;
-          <input type='text' ref={inputRef} defaultValue={defaultNewServerName} key={defaultNewServerName} />
-          &nbsp;
-        </span>
-        <span className='server-manager-purchase-server-button'
-          onClick={() => {
-            'use getPurchasedServerLimit';
-            'use purchaseServer';
-
-            if (!inputRef.current) throw new Error('Something went wrong, no input ref');
-            const newServer = ns.purchaseServer(inputRef.current.value, 2);
-            if (!newServer && servers.length == ns.getPurchasedServerLimit()) {
-              ns.toast('Server limit has been reached', 'error');
-              return;
-            } else if (!newServer) {
-              ns.toast('Not enough money', 'error');
-              return;
-            }
-            ns.toast(`Successfully bought server '${newServer}'`, 'success');
-            setServers(getPurchasedServers(ns));
-          }}
-        >OK</span>
-      </div>
-      <div className='server-manager'>
-        {React.createElement(List,)}
-        <List li={ServerEntry} data={servers.map(s => ({
-          ns,
-          server: s,
-          remove: () => {
-            'use deleteServer';
-            ns.deleteServer(s.hostname);
-            setServers(getPurchasedServers(ns));
-          }
-        }))}></List>
-      </div>
-    </div>
-  </>;
+  return WindowApp.mount(<ServerManager ns={ns}></ServerManager>);
 }
