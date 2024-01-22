17f19cca40764f0f472c1cdb9389f75853f50df6
Nils Ramstoeck <nilsramstoeck@gmail.com>
2024-01-22 12:40:04 +0000

    create drag and drop target

---
+++ src/lib/components/DragTarget.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  group: string;
+  data: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function DragTarget({ group, data, onDragStart, ...attr }: Props) {
+  return <div
+    draggable={true}
+    id={`drag-target-${Date.now()}`}
+    data-drag-group={group}
+    data-drag-data={data}
+    onDragStart={(e) => {
+      e.dataTransfer.setData(group, e.currentTarget.id);
+      e.dataTransfer.setData('data', data);
+      if (onDragStart) onDragStart(e);
+    }}
+    {...attr}
+  ></div>;
+}
---
+++ src/lib/components/DropTarget.tsx
@@ -0,0 +1,22 @@
+import { log } from "console";
+import React from "react";
+
+type Props = {
+  accept: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function DropTarget({ accept, onDragEnter, onDragOver, ...attr }: Props) {
+  return <div
+    draggable={true}
+    data-drag-accept={accept}
+    onDragEnter={(e) => {
+      if (e.dataTransfer.types.includes(accept)) e.preventDefault();
+      if (onDragEnter) onDragEnter(e);
+    }}
+    onDragOver={(e) => {
+      if (e.dataTransfer.types.includes(accept)) e.preventDefault();
+      if (onDragOver) onDragOver(e);
+    }}
+    {...attr}
+  ></div>;
+}
