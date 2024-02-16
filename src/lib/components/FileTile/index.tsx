15a37dda26c7ff94f442b3970c07e04a3b87efaa
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-02-16 23:01:53 +0100

    fix styling a little

--- src/lib/components/FileTile/index.tsx
+++ src/lib/components/FileTile/index.tsx
@@ -58,27 +58,23 @@ export function FileTile({ file, path }: Props) {
     <Style></Style>
     <Icon></Icon>
     <div
+      className='file-name'
       spellCheck={false}
       onDoubleClick={(e) => {
         e.stopPropagation();
-
         const el = e.currentTarget as HTMLDivElement;
         el.contentEditable = 'true';
         el.focus();
-
         const keydownHandler = (e: KeyboardEvent) => {
           if (e.key == 'Enter') {
             el.contentEditable = 'false';
             window.focus();
           }
         };
-
         el.addEventListener('keydown', keydownHandler);
-
         el.addEventListener('focusout', ({ currentTarget }) => {
           const newName = (currentTarget as HTMLDivElement).textContent;
           if (newName == file.name) return;
-
           try {
             if (type == 'folder')
               moveFolder(ns, `${path}/${file.name}`, `${path}/${newName}`);
@@ -86,19 +82,15 @@ export function FileTile({ file, path }: Props) {
               moveFile(ns, `${path}/${file.name}`, `${path}/${newName}`);
           } catch (e) {
             console.log({ e });
-
             ns.toast((e as Error).name, 'error');
           }
-
           el.removeEventListener('keydown', keydownHandler);
-
         }, { once: true });
-
       }}>{file.name}</div>
     {['js', 'txt', 'folder'].includes(type) ?
       <FontAwesomeIcon
         icon={faTrashCan}
-        style={{ cursor: 'pointer', fontSize: '0.8em' }}
+        style={{ cursor: 'pointer', fontSize: '0.9em' }}
         onClick={async () => {
           if (! await ns.prompt(`Are you sure you want to delete ${path}/${file.name}?`, { type: 'boolean' })) return;
           if (type == 'folder')
