979dd3908c58e93d370d23ae64dc7185160eaa50
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-02-16 23:50:53 +0100

    fix memory leak

--- src/lib/components/FileTile/index.tsx
+++ src/lib/components/FileTile/index.tsx
@@ -101,6 +101,7 @@ export function FileTile({ file, path }: Props) {
             if (!term.terminalInput) return ns.toast('Editing can only be triggered when the terminal tab is selected', 'warning');
             term.exec(getConnectionPath(ns, server).reduce((prev, cur) => prev + `connect ${cur};`, ''));
             term.exec(`nano ${filepath}`);
+            term.cleanup();
           }}></FontAwesomeIcon> : undefined}
       {['js', 'txt', 'folder'].includes(type) ?
         <FontAwesomeIcon
