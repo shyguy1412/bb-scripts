fb02c21aa597b0d2851361e8144d536c871bdd5f
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-03-13 19:26:32 +0100

    add svg module

--- src/module.d.ts
+++ src/module.d.ts
@@ -2,3 +2,8 @@ declare module '*.css' {
   const Style: import('react').FunctionComponent;
   export default Style;
 }
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
