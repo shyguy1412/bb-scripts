4af1bc8c137f2fd25c4c571617bfe260954fbde7
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-03-14 14:00:55 +0100

    improve exp farmer

--- src/servers/home/ExpFarm.ts
+++ src/servers/home/ExpFarm.ts
@@ -1,23 +1,38 @@
-import { fullyWeakenServer } from '@/lib/Hack';
 import { allocateRam, getRamCost } from '@/lib/System';
+import { getAllServers as getAllServersUnsafe } from '@/lib/Network' with {type: 'unsafe'};
 
 export async function main(ns: NS) {
-  const target = ns.args[0] as string ?? 'foodnstuff';
-
-  await fullyWeakenServer(ns, target);
-
-  while (true) { 
-    const server = await allocateRam(ns, {
+  while (true) {
+    const [target, hosts] = await allocateRam(ns, {
       ram: getRamCost(ns, [
         "getServer",
-        "getHostname",
+        "scan",
       ]),
-    }, ns => { 
-      return ns.getServer(ns.getHostname());
+    }, ns => {
+      return [
+        ns
+          .getServer(ns.args[0] as string ?? 'foodnstuff'),
+        getAllServersUnsafe(ns)
+          .map(s => ns.getServer(s))
+          .filter(s => !s.purchasedByPlayer && s.hostname != 'home' && s.hasAdminRights && s.maxRam)
+      ] as const;
     });
-    if (!server) throw new Error("could not get server"); 
-    const ram = getRamCost(ns, ["grow"]);
-    const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
-    await allocateRam(ns, { ram, threads }, ns => ns.grow(target));
+    if (!hosts.length) throw new Error("could not get hosts");
+
+    if (target.hackDifficulty != target.minDifficulty) {
+
+      await Promise.all(hosts.map(server => {
+        const ram = getRamCost(ns, ["weaken"]);
+        const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
+        return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.weaken(target.hostname));
+      }));
+    } else {
+      await Promise.all(hosts.map(server => {
+        const ram = getRamCost(ns, ["grow"]);
+        const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
+        return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.grow(target.hostname));
+      }));
+    }
+
   }
 }
