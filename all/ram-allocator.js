01a2bf7eada574ee56dda89513a4d85ffc14aab0
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-03-14 13:59:37 +0100

    have ram-allocator report after ram is free

--- all/ram-allocator.js
+++ all/ram-allocator.js
@@ -1,5 +1,6 @@
 export async function main(ns) {
   const [task, report] = globalThis['ns-' + ns.pid];
   delete globalThis['ns-' + ns.pid];
-  report(await task(ns));
+  const value = await task(ns);
+  ns.atExit(() => report(value));
 }
