719a0168286a4841b71d5fe09ba795eda8017280
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-03-15 11:00:52 +0100

    prevent crashes on low ram

--- src/servers/home/ExpFarm.ts
+++ src/servers/home/ExpFarm.ts
@@ -14,20 +14,20 @@ export async function main(ns: NS) {
           .getServer(ns.args[0] as string ?? 'foodnstuff'),
         getAllServersUnsafe(ns)
           .map(s => ns.getServer(s))
-          .filter(s => !s.purchasedByPlayer && s.hostname != 'home' && s.hasAdminRights && s.maxRam)
+          .filter(s => s.hostname != 'home' && s.hasAdminRights && s.maxRam)
       ] as const;
     });
     if (!hosts.length) throw new Error("could not get hosts");
 
     if (target.hackDifficulty != target.minDifficulty) {
 
-      await Promise.all(hosts.map(server => {
+      await Promise.allSettled(hosts.map(server => {
         const ram = getRamCost(ns, ["weaken"]);
         const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
         return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.weaken(target.hostname));
       }));
     } else {
-      await Promise.all(hosts.map(server => {
+      await Promise.allSettled(hosts.map(server => {
         const ram = getRamCost(ns, ["grow"]);
         const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
         return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.grow(target.hostname));
