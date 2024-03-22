abb48efce36e1e8ed550ffee8aff200489890d2c
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-03-22 17:06:27 +0100

    refactor prep server

--- src/servers/home/PrepServer.ts
+++ src/servers/home/PrepServer.ts
@@ -1,8 +1,9 @@
 import { allocateRam, getRamCost } from '@/lib/System';
 import { getAllServers as getAllServersUnsafe } from '@/lib/Network' with {type: 'unsafe'};
+import { prepServer } from '@/lib/Hack';
 
-export function autocomplete({servers}:{servers:string[]}){
-  return servers
+export function autocomplete({ servers }: { servers: string[]; }) {
+  return servers;
 }
 
 export async function main(ns: NS) {
@@ -15,29 +16,17 @@ export async function main(ns: NS) {
     }, ns => {
       return [
         ns
-          .getServer(ns.args[0] as string ?? 'foodnstuff'),
+          .getServer(ns.args[0] as string),
         getAllServersUnsafe(ns)
           .map(s => ns.getServer(s))
-          .filter(s => s.hostname != 'home' && s.hasAdminRights && s.maxRam)
+          .filter(s => s.hasAdminRights && s.maxRam)
+          .map(s => s.hostname)
       ] as const;
     });
     if (!hosts.length) throw new Error("could not get hosts");
 
     if (target.hackDifficulty == target.minDifficulty && target.moneyAvailable == target.moneyMax) return;
+    await prepServer(ns, target.hostname, hosts);
 
-    if (target.hackDifficulty != target.minDifficulty) {
-
-      await Promise.all(hosts.map(server => {
-        const ram = getRamCost(ns, ["weaken"]);
-        const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
-        return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.weaken(target.hostname));
-      }));
-    } else {
-      await Promise.all(hosts.map(server => {
-        const ram = getRamCost(ns, ["grow"]);
-        const threads = Math.floor((server.maxRam - server.ramUsed) / ram) || 1;
-        return allocateRam(ns, { host: hosts.map(s => s.hostname), ram, threads }, ns => ns.grow(target.hostname));
-      }));
-    }
   }
 }
