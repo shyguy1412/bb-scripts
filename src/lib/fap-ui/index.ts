2ca8ade56a2ce26e3bd7c352720ee5e88fa9ea32
Nils Ramstöck <nilsramstoeck@gmail.com>
2024-08-16 19:53:52 +0200

    refactor FapUI

--- src/lib/fap-ui/index.ts
+++ src/lib/fap-ui/index.ts
@@ -1,27 +1,21 @@
-import type _React from 'NetscriptDefinitions';
 import React, { useReducer } from 'react';
 
 type ReactEventType = keyof Omit<React.DOMAttributes<any>, 'children' | 'dangerouslySetInnerHTML'>;
 type ReactEvent<K extends ReactEventType> = Parameters<NonNullable<React.DOMAttributes<any>[K]>>[0];
-
-export type FapElement<T> = React.JSX.Element & FapEvents<T> & FapModifier<T> & Attributes<T>;
 
 export type FapEvents<T> = {
   [key in ReactEventType]: (cb: (el: FapElement<T>, ev: ReactEvent<key>) => void) => FapElement<T>;
 };
 
 type AttributeSetter<T> = (attr: string | number | boolean) => FapElement<T>;
-
 export type Attributes<T> = HTMLAttributes<T> & AriaAttributes<T> & DataAttributes<T>;
 
 type HTMLAttributeKeys<T> = Capitalize<keyof Omit<React.AllHTMLAttributes<T>, keyof FapEvents<T> | keyof React.AriaAttributes | 'className'> | 'class'>;
-
 export type HTMLAttributes<T> = Required<{
   [key in HTMLAttributeKeys<T>]: AttributeSetter<T>;
 }>;
 
 type AriaAttributeKeys = keyof React.AriaAttributes extends `aria-${infer A}` ? `Aria${Capitalize<A>}` : never;
-
 export type AriaAttributes<T> = {
   [key in AriaAttributeKeys]: AttributeSetter<T>
 };
@@ -30,10 +24,18 @@ export type DataAttributes<T> = {
   [key in `Data${Capitalize<string>}`]: AttributeSetter<T>
 };
 
+export type FapElement<T> = React.JSX.Element & FapEvents<T> & FapModifier<T> & Attributes<T>;
+
+export type FapComponent<T> = (content?: React.ReactNode) => FapElement<T>;
+
+export type FapComponents = {
+  [key in Capitalize<keyof HTMLElementTagNameMap>]: FapComponent<HTMLElementTagNameMap[Uncapitalize<key>]>
+};
+
+
 export type FapModifier<T> = {
   Content: (newContent: React.ReactNode) => FapElement<T>;
   Style: (style: React.CSSProperties) => FapElement<T>;
-  Update: () => FapElement<T>;
 };
 
 export type FapState<T> = {
@@ -49,8 +51,7 @@ export type Fapped<T> = {
   render?: () => void;
 };
 
-export function useFap<T extends {}>(state: T): Fapped<T> {
-
+function useFap<T extends {}>(state: T): Fapped<T> {
   const fap: Fapped<T> = {
     state: new Proxy(state, {
       set(target, p, newValue) {
@@ -58,42 +59,18 @@ export function useFap<T extends {}>(state: T): Fapped<T> {
         return Reflect.set(target, p, newValue);
       },
     }),
-    bind: () => {
-
-      const [, render] = useReducer(() => {
-        return {};
-      }, {});
-
-      fap.render = render;
-
-      return fap.state;
-    }
+    bind: () => (fap.render = useReducer(_ => ({}), {})[1], fap.state)
   };
-
   return fap;
-
 };
 
-export function isEventHandler(p: string): p is ReactEventType {
+function isEventHandler(p: string): p is ReactEventType {
   return /^on[A-Z]/.test(p);
 }
 
-type Keys<T extends Record<string, any>, U extends PropertyKey[] = []> =
-  {
-    [P in keyof T]: {} extends Omit<T, P> ? [P] : [P, ...Keys<Omit<T, P>, U>]
-  }[keyof T];
-
-export function isModifierProp(p: string): p is keyof FapModifier<any> {
-  const modifierProps: Keys<FapModifier<any>> = ['Style', 'Content', 'Update'];
-
-  return modifierProps.includes(p as any);
+function isModifierProp(p: string): p is keyof FapModifier<any> {
+  return p == 'Content' || p == 'Style';
 }
-
-export type FapComponent<T> = (content?: React.ReactNode) => FapElement<T>;
-
-type FapComponents = {
-  [key in Capitalize<keyof HTMLElementTagNameMap>]: FapComponent<HTMLElementTagNameMap[Uncapitalize<key>]>
-};
 
 export const FapComponents = new Proxy({} as FapComponents, {
   get(_, p) {
@@ -105,11 +82,9 @@ export const FapComponents = new Proxy({} as FapComponents, {
       const { state, ...fap } = useFap<FapState<any>>({
         events: {},
         attributes: {},
-        content: undefined,
+        content,
         style: {}
       });
-
-      state.content = content;
 
       const Component = () => {
         const {
@@ -131,11 +106,6 @@ export const FapComponents = new Proxy({} as FapComponents, {
               return (cb: any) => (state['events'][p] = cb.bind(undefined, receiver), receiver);
             }
 
-            if (isModifierProp(p) && p == 'Update') {
-              if (fap.render) fap.render();
-              return receiver;
-            }
-
             if (isModifierProp(p)) {
               return (value: any) => (state[p.toLocaleLowerCase() as Lowercase<typeof p>] = value, receiver);
             }
@@ -152,7 +122,7 @@ export const FapComponents = new Proxy({} as FapComponents, {
             return Reflect.get(target, p);
           },
         }
-      ) as any;
+      );
     };
   },
 });
@@ -165,40 +135,19 @@ type Bind<T> = Required<{
   ]
 }>;
 
-export function createBinding<T extends (...args: any[]) => R, R extends FapElement<any>, A extends Parameters<T>>(component: T, ...args: A) {
+export function createBinding<T extends (...args: any[]) => FapElement<any>, A extends Parameters<T>>(component: T, ...args: A): [React.JSX.Element, ...Bind<A>] {
 
-  const store = [...args];
+  const { state, bind } = useFap(args);
+  const Wrapper = () => component(...bind());
 
-  const binding = new Proxy(args as any, {
-    get(target, p) {
-      if (typeof p == 'symbol') {
-        return Reflect.get(target, p);
-      }
-
-      if (isNaN(p as any)) {
-        return Reflect.get(target, p);
-      }
-
-      return [
-        () => {
-          return store[p as any];
-        },
-        (val: any) => {
-          store[p as any] = val;
-          if (state.render) state.render();
-        }
-      ];
+  const binding = args.map((_, i) => [
+    () => {
+      return state[i];
     },
-  }) as Bind<A>;
+    (val: any) => {
+      state[i] = val;
+    }
+  ]) as Bind<A>;
 
-  const state = useFap(store);
-
-  const Wrapper = () => {
-
-    state.bind();
-
-    return component(...store);
-  };
-
-  return [React.createElement(Wrapper) as _React.ReactNode, ...binding] as const;
+  return [React.createElement(Wrapper), ...binding] as const;
 }
