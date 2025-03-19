6c33264f3382af0d23a8babf229997c9280e9ecd
Shy <nilsramstoeck@gmail.com>
2025-03-19 10:40:49 +0100

    move bindings to submodule

--- src/servers/home/rust/bindings/src/types/any.rs
+++
@@ -1,9 +0,0 @@
-use wasm_bindgen::JsValue;
-
-pub struct Any(pub JsValue, pub JsValue);
-
-impl Into<Any> for &str {
-  fn into(self) -> Any {
-      return Any(JsValue::from(self), JsValue::from(self));
-  }
-}
