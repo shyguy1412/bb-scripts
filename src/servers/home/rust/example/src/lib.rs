3a12761a7937728a84419e5eff16c01bea048067
Shy <shyguyx1412@gmail.com>
2025-03-19 20:59:49 +0000

    update bindings

--- src/servers/home/rust/example/src/lib.rs
+++ src/servers/home/rust/example/src/lib.rs
@@ -4,7 +4,6 @@ use wasm_bindgen::JsValue;
 #[bb_bindgen]
 pub fn main(ns:NS) -> Result<(), JsValue>{
   let message: String = ns.args.get(&"0")?;
-  let message: std::string::String = message.into();
-  ns.tprint(message.as_str())?;
+  ns.tprint(message)?;
   Ok(())
 }
