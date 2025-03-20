40705260809f2e54026a321e46958b6fecab8ca4
Shy <shyguyx1412@gmail.com>
2025-03-20 21:00:34 +0000

    try await, fail, cry

--- src/servers/home/rust/example/src/lib.rs
+++ src/servers/home/rust/example/src/lib.rs
@@ -2,8 +2,25 @@ use bitburner_bindings::*;
 use wasm_bindgen::JsValue;
 
 #[bb_bindgen]
-pub fn main(ns:NS) -> Result<(), JsValue>{
-  let message: String = ns.args.get(&"0")?;
-  ns.tprint(message)?;
-  Ok(())
+pub async fn main(ns: NS) -> Result<(), JsValue> {
+    let message: String = ns.args.get(&"0")?;
+    ns.tprint(message)?;
+
+    let promise = ns.sleep(Number::new(
+        JsValue::from_f64(5000f64),
+        JsValue::undefined(),
+    ))?;
+
+    let any:Any = promise.into();
+
+    any.await?;
+
+    // let jsval:JsValue = Into::<Any>::into(promise).clone();
+
+    // wasm_bindgen_futures::JsFuture::from(js_sys::Promise::from(jsval.clone())).await?;
+
+    ns.tprint(String::new(JsValue::from_str("Resolved uwu :3"), JsValue::undefined()))?;
+
+    // Err(jsval)
+    Ok(())
 }
