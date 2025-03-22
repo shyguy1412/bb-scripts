50629ca74c260abc49a5cebbe4ec6b04d7f508f0
Shy <shyguyx1412@gmail.com>
2025-03-22 23:02:26 +0000

    update bindings and example to show off closure fuckery

--- src/servers/home/rust/example/src/lib.rs
+++ src/servers/home/rust/example/src/lib.rs
@@ -2,25 +2,38 @@ use bitburner_bindings::*;
 use wasm_bindgen::JsValue;
 
 #[bb_bindgen]
-pub async fn main(ns: NS) -> Result<(), JsValue> {
+pub async fn main(ns: &'static NS) -> Result<(), JsValue> {
+    let closure = js_closure!(|args: Any| -> Result<Any, JsValue> {
+        ns.tprint("message".into())?;
+        Ok(args)
+    });
+
+    ns.atExit(closure, "closure".into())?;
+    ns.atExit(js_closure!(logMessage), "closure".into())?;
+
     let message: String = ns.args.get(&"0")?;
+
     ns.tprint(message)?;
+    let promise = ns.sleep(1000f64.into())?;
+    log("I am a log test");
 
-    let promise = ns.sleep(Number::new(
-        JsValue::from_f64(5000f64),
-        JsValue::undefined(),
-    ))?;
+    promise.await?;
 
-    let any:Any = promise.into();
+    ns.tprint("Resolved uwu :3".into())?;
 
-    any.await?;
 
-    // let jsval:JsValue = Into::<Any>::into(promise).clone();
-
-    // wasm_bindgen_futures::JsFuture::from(js_sys::Promise::from(jsval.clone())).await?;
-
-    ns.tprint(String::new(JsValue::from_str("Resolved uwu :3"), JsValue::undefined()))?;
-
-    // Err(jsval)
     Ok(())
+}
+
+fn logMessage(args: Any) -> Result<Any, JsValue> {
+    log("I am a global function");
+    Ok(Any::new(JsValue::undefined(), JsValue::undefined()))
+}
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
